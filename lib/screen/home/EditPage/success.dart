9ef12023bd21d52a41a2845bfae8dfe5e575bc0c
GN <sharmashalu3148@gmail.com>
2024-06-13 09:19:29 +0530

    13/04/24

--- lib/screen/home/EditPage/success.dart
+++ lib/screen/home/EditPage/success.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../utils/global_var.dart';
+
 class EditScreen extends StatefulWidget {
   const EditScreen({super.key});
 
@@ -11,8 +13,24 @@ class _EditScreenState extends State<EditScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Success Quotes'),
+      body: Wrap(
+        children: [
+          ...List.generate(imgTheme.length, (index) => buildContainer(img: imgTheme[selectedIndex]['img']))
+        ],
+      ),
+    );
+  }
+
+  Container buildContainer({required String img}) {
+    return Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blue,width: 1),
+        color: Colors.blue,
+        image: DecorationImage(
+          image: AssetImage(img),
+        ),
       ),
     );
   }
--- lib/utils/global_var.dart
+++ lib/utils/global_var.dart
@@ -109,3 +109,186 @@ List love = [
     "author": "Oscar Wilde"
   }
 ];
+List friendShip = [
+  {
+    "quote":
+        "Friendship is born at that moment when one person says to another: 'What! You too? I thought I was the only one.'",
+    "author": "C.S. Lewis"
+  },
+  {
+    "quote": "A friend is someone who knows all about you and still loves you.",
+    "author": "Elbert Hubbard"
+  },
+  {
+    "quote":
+        "Friendship is the only cement that will ever hold the world together.",
+    "author": "Woodrow Wilson"
+  },
+  {
+    "quote":
+        "Walking with a friend in the dark is better than walking alone in the light.",
+    "author": "Helen Keller"
+  },
+  {
+    "quote":
+        "A true friend is someone who is there for you when he'd rather be anywhere else.",
+    "author": "Len Wein"
+  },
+];
+List motivational = [
+  {
+    "quote": "The only way to do great work is to love what you do.",
+    "author": "Steve Jobs"
+  },
+  {
+    "quote": "Believe you can and you're halfway there.",
+    "author": "Theodore Roosevelt"
+  },
+  {
+    "quote":
+        "The future belongs to those who believe in the beauty of their dreams.",
+    "author": "Eleanor Roosevelt"
+  },
+  {
+    "quote":
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "author": "Winston Churchill"
+  },
+  {
+    "quote": "Your time is limited, don't waste it living someone else's life.",
+    "author": "Steve Jobs"
+  },
+];
+List inspirational = [
+  {
+    "quote":
+        "The only way to achieve the impossible is to believe it is possible.",
+    "author": "Charles Kingsleigh"
+  },
+  {
+    "quote": "In the middle of difficulty lies opportunity.",
+    "author": "Albert Einstein"
+  },
+  {
+    "quote":
+        "The only limit to our realization of tomorrow will be our doubts of today.",
+    "author": "Franklin D. Roosevelt"
+  },
+  {
+    "quote":
+        "Keep your face always toward the sunshine—and shadows will fall behind you.",
+    "author": "Walt Whitman"
+  },
+  {
+    "quote": "The best way to predict the future is to create it.",
+    "author": "Abraham Lincoln"
+  },
+];
+List happiness = [
+  {
+    "quote":
+        "Happiness is not something ready made. It comes from your own actions.",
+    "author": "Dalai Lama"
+  },
+  {
+    "quote": "The only joy in the world is to begin.",
+    "author": "Cesare Pavese"
+  },
+  {"quote": "Happiness is a warm puppy.", "author": "Charles M. Schulz"},
+  {
+    "quote":
+        "The grand essentials of happiness are: something to do, something to love, and something to hope for.",
+    "author": "George Washington Burnap"
+  },
+  {
+    "quote":
+        "Happiness is when what you think, what you say, and what you do are in harmony.",
+    "author": "Mahatma Gandhi"
+  },
+];
+List sad = [
+  {
+    "quote":
+        "The word 'happiness' would lose its meaning if it were not balanced by sadness.",
+    "author": "Carl Jung"
+  },
+  {
+    "quote":
+        "The walls we build around us to keep sadness out also keeps out the joy.",
+    "author": "Jim Rohn"
+  },
+  {
+    "quote": "Sadness flies away on the wings of time.",
+    "author": "Jean de La Fontaine"
+  },
+  {
+    "quote":
+        "The way sadness works is one of the strange riddles of the world.",
+    "author": "Lemony Snicket"
+  },
+  {
+    "quote":
+        "Behind every sweet smile, there is a bitter sadness that no one can ever see and feel.",
+    "author": "Toba Beta"
+  },
+];
+List success = [
+  {
+    "quote":
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "author": "Winston Churchill"
+  },
+  {
+    "quote":
+        "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    "author": "Albert Schweitzer"
+  },
+  {
+    "quote": "The only way to do great work is to love what you do.",
+    "author": "Steve Jobs"
+  },
+  {
+    "quote":
+        "Success is walking from failure to failure with no loss of enthusiasm.",
+    "author": "Winston Churchill"
+  },
+  {
+    "quote": "Success is not in what you have, but who you are.",
+    "author": "Bo Bennett"
+  },
+];
+List imgTheme = [
+  {
+    'img': 'assets/img/1.jpg',
+  },
+  {
+    'img': 'assets/img/1.jpg',
+  },
+  {
+    'img': 'assets/img/1.jpg',
+  },
+  {
+    'img': 'assets/img/1.jpg',
+  },
+  {
+    'img': 'assets/img/1.jpg',
+  },
+  {
+    'img': 'assets/img/2.jpg',
+  },
+  {
+    'img': 'assets/img/2.jpg',
+  },
+  {
+    'img': 'assets/img/2.jpg',
+  },
+  {
+    'img': 'assets/img/2.jpg',
+  },
+  {
+    'img': 'assets/img/2.jpg',
+  },
+  {
+    'img': 'asses/img/3.jpg',
+  },
+];
